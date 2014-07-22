5e290981118c7c6492cfa1b3bc5d3eb9b9f4fef4
heatherelli <heather.ellithorpe@gmail.com>
2014-07-22 12:52:44 +0000

    this is my first commit

---
+++ W2_MON_ROCK_PAPER_SCISSOR.rb
@@ -0,0 +1,14 @@
+
+def rock_paper_scissor(rock,paper,scissor)
+    
+        x=rock
+        y=paper
+        z=scissor
+    
+    xyz = #{rock * paper * scissor}.random
+
+end
+
+    puts xyz.methods
+
+    
